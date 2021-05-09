d0bc06b378566a1c7abe6fdbbd4fe5a88d86b937
JihyeCha <83751278+JihyeCha@users.noreply.github.com>
2021-05-09 23:40:23 +0900

    Create 5-4.py

---
+++ 5-4.py
@@ -0,0 +1,20 @@
+# 조건문 if else, elif, else
+
+a = 3
+b = 2
+c = 5
+d = c - a
+
+
+if a > b:            # a가 b 보다 크면 Hello 출력
+   print("Hello")
+   
+if a > c:            # a가 c 보다 크면 What? 출력
+    print("What?")
+    
+elif a == d:         # a가 3과 같다면 Bye! 출력    
+    print("Bye!")
+    
+else:                # a가 b보다 작으면 Hi!출력
+    print("Hi!")    
+    
