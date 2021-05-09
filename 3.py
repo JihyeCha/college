250c60443fdda2f1b7910982c8b8adcba7ace682
JihyeCha <83751278+JihyeCha@users.noreply.github.com>
2021-05-09 22:51:16 +0900

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,13 @@
+x = "Hello"                # 변수 x에 문자 Hello를 선언
+y = 'Bye'                  # 변수 y 에 문자 Bye를 선언
+z = """안녕하세요.
+저는 21학번 입니다"""       # 변수 z에 문자 2행을 선언
+
+
+print(x)
+print(y)
+print(z)
+print("\n")                # \n은 공백
+print(x+y+z)
+print("\n")                # \n 은 공백
+print(z)
