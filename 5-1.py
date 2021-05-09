be52988be050dd38e441268ba24610ce8f2d6a8d
JihyeCha <83751278+JihyeCha@users.noreply.github.com>
2021-05-09 23:04:04 +0900

    Create 5-1.py

---
+++ 5-1.py
@@ -0,0 +1,14 @@
+# 블리안 : Boolean - 조건에 따라
+
+x = True
+y = False
+
+print(x), print(y)
+print("\n")              # \n은 공백.
+
+a = 3
+b = 4
+
+if a > b :               # if : 만약 ~이라면
+    print("a가 크다.")
+print("False이기 때문에 출력이 안된다", "\n")
