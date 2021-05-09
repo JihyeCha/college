9890f3cf9b686c0c2e4e98b8a25fa85f00cd7d34
JihyeCha <83751278+JihyeCha@users.noreply.github.com>
2021-05-10 00:51:42 +0900

    Create J1-3.py

---
+++ J1-3.py
@@ -0,0 +1,12 @@
+pay_rate = int(input("시급 입력: "))
+hour_worked = int(input("일한 시간 입력: "))
+monthly_pay = pay_rate * hour_worked
+print(monthly_pay)
+
+#기본 a = 1150, b = 50000, c = a x b    / print(c)
+#고급 won = int, exchange_rate = in, tran_rate( * ), print(tran_rate)
+
+won = int(input("원화 입력: "))
+exchange_rate = int(input("달러 입력: "))
+tran_rate = won * exchange_rate
+print(tran_rate)
